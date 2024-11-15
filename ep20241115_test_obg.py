60139136fc20afc727089fd073a95558ebac8f16
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-15 08:01:38 +0500

    Смотрю переопределение методов

---
+++ ep20241115_test_obg.py
@@ -0,0 +1,15 @@
+"""Изучаю переопределение методов и ООП  трогаю лапкой
+"""
+class Person:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+   
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
+
+p = Person("aaa", "bbb")
+
+print(p)
+print(p.first_name, p.last_name)
